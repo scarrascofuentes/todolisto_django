d018b5672f1f7a8f9dc094d4081dc0337437ce78
Unknown <hm.astorga@gmail.com>
2018-06-15 22:59:42 -0400

    basura

--- main/urls.py
+++ main/urls.py
@@ -1,11 +1,13 @@
 
-from django.urls import path
+from django.urls import path, include
 from . import views
 from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
     path('login',auth_views.LoginView.as_view(), name="login"),
     path('logout',auth_views.LogoutView.as_view(), name="logout"),
     path('home', views.index, name= "home"),
+   
   
 ]
--- todolisto/urls.py
+++ todolisto/urls.py
@@ -19,4 +19,7 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('todolisto/', include('main.urls')),
+   
+    
+   
 ]
