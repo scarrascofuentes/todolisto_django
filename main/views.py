4632e813dfc81b1ffca9068b13967c083afd66fb
Unknown <hm.astorga@gmail.com>
2018-06-16 12:58:43 -0400

    feat: se añade el método que carga el archivo html de las tareas

--- main/views.py
+++ main/views.py
@@ -6,3 +6,7 @@ from django.contrib.auth.decorators import login_required
 @login_required()
 def index(request):
     return render(request, 'home.html')
+
+@login_required()
+def tareas(request):
+    return render(request, "tareas.html")
