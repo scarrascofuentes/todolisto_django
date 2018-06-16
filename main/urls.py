4bf68ee3aab727d918a2c5b5104923366f66c28a
Unknown <hm.astorga@gmail.com>
2018-06-15 20:41:00 -0400

    FIX: arreglo de vista de logout
    para logout se estaba usando LoginView, en vez de LogoutView

--- main/urls.py
+++ main/urls.py
@@ -5,7 +5,7 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('login',auth_views.LoginView.as_view(), name="login"),
-    path('logout',auth_views.LoginView.as_view(), name="logout"),
+    path('logout',auth_views.LogoutView.as_view(), name="logout"),
     path('home', views.index, name= "home"),
   
 ]
