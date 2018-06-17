b16f88bdb00881dd9687c775550ff7afe2e17033
Unknown <hm.astorga@gmail.com>
2018-06-16 20:18:43 -0400

    Feat: se crea el método crear_tarea
    también se logra obtener las tareas de la BD y guardar otras nuevas.

--- main/views.py
+++ main/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
+from .models import Tarea
 
 # Create your views here.
 @login_required()
@@ -9,4 +10,16 @@ def index(request):
 
 @login_required()
 def tareas(request):
-    return render(request, "tareas.html")
+    tareas = Tarea.objects.all()
+    return render(request, "tareas.html", { 'tareas' : tareas})
+
+@login_required()
+def crear_tarea(request):
+    if request.method == 'POST':
+        tarea = Tarea()
+        tarea.titulo = request.POST.get('titulo_tarea')
+        tarea.descripcion = request.POST.get('descripcion_tarea')
+        #tarea.usuario = request.user
+        tarea.save()
+    tareas = Tarea.objects.all()
+    return render(request, "tareas.html", { 'tareas' : tareas})
