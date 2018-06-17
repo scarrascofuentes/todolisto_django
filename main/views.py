8c6d9a7af063da2f51254fb1279a221d4d9e45a7
Unknown <hm.astorga@gmail.com>
2018-06-17 00:15:20 -0400

    Feat: definición de método de registro
    aún no funciona

--- main/views.py
+++ main/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login, authenticate
 from .models import Tarea
 
 # Create your views here.
@@ -23,3 +25,17 @@ def crear_tarea(request):
         tarea.save()
     tareas = Tarea.objects.all()
     return render(request, "tareas.html", { 'tareas' : tareas})
+
+def registro(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/signup.html', {'form': form})
