53697ebbd2cd1a232e6b5b08a6ca3e1a7618b3a5
Héctor Astorga <hm.astorga@gmail.com>
2018-06-17 22:31:01 -0400

    Fix: a la ruta register ahora se le carga una clase de views

--- main/urls.py
+++ main/urls.py
@@ -7,7 +7,7 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('login',auth_views.LoginView.as_view(), name="login"),
     path('logout',auth_views.LogoutView.as_view(), name="logout"),
-    path('register',views.registro, name="register"),
+    path('register', views.RegistroUsuario.as_view(), name="register"),
     path('home', views.index, name= "home"),
     path('tareas', views.tareas, name= "tareas"),
     path('crear_tarea', views.crear_tarea, name= "crear_tarea"),
