29a8b44476d1967d0e49345df915f073a30cedc0
scarrascofuentes <samuel.carrasco.f@mail.pucv.cl>
2018-06-20 05:51:44 -0300

    feat: Se agrega modelo TipoTarea

--- main/models.py
+++ main/models.py
@@ -2,8 +2,16 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+class TipoTarea (models.Model):
+    titulo = models.CharField(max_length = 255)
 
 class Tarea (models.Model):
     titulo = models.CharField(max_length = 255)
     descripcion = models.CharField(max_length = 255)
     usuario = models.ForeignKey(User, on_delete = models.CASCADE)
+    tipo = models.ForeignKey(TipoTarea, on_delete = models.CASCADE)
+
+    
+
+
+
