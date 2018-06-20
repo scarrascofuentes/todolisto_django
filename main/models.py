e59d675f57a11a764cb72fa4358297679bf3411f
scarrascofuentes <samuel.carrasco.f@mail.pucv.cl>
2018-06-20 06:07:57 -0300

    fix: Se cambia el campo 'titulo' a 'nombre'

--- main/models.py
+++ main/models.py
@@ -3,7 +3,10 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class TipoTarea (models.Model):
-    titulo = models.CharField(max_length = 255)
+    nombre = models.CharField(max_length = 255)
+
+    def __str__(self):
+	    return '{}'.format(self.nombre)
 
 class Tarea (models.Model):
     titulo = models.CharField(max_length = 255)
