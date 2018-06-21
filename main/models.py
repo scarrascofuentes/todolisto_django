c60fd1f5237372aef7655ad1a69b5e7bd9432ebf
Héctor <hm.astorga@gmail.com>
2018-06-20 22:29:49 -0400

    Feat: Se agregan con éxito los cambios

--- main/models.py
+++ main/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+
 class TipoTarea (models.Model):
     nombre = models.CharField(max_length = 255)
 
@@ -13,8 +14,3 @@ class Tarea (models.Model):
     descripcion = models.CharField(max_length = 255)
     usuario = models.ForeignKey(User, on_delete = models.CASCADE)
     tipo = models.ForeignKey(TipoTarea, on_delete = models.CASCADE)
-
-    
-
-
-
