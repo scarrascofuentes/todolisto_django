7840cf9597e87bbb709a70ef4dba2ec7dcd64f83
Héctor <hm.astorga@gmail.com>
2018-06-21 00:30:40 -0400

    Feat: Se crea el modelo EstadoTarea

--- main/models.py
+++ main/models.py
@@ -9,8 +9,16 @@ class TipoTarea (models.Model):
     def __str__(self):
 	    return '{}'.format(self.nombre)
 
+class EstadoTarea (models.Model):
+    nombre = models.CharField(max_length = 255)
+
+    def __str__(self):
+	    return '{}'.format(self.nombre)
+
+
 class Tarea (models.Model):
     titulo = models.CharField(max_length = 255)
     descripcion = models.CharField(max_length = 255)
     usuario = models.ForeignKey(User, on_delete = models.CASCADE)
     tipo = models.ForeignKey(TipoTarea, on_delete = models.CASCADE)
+    estado = models.ForeignKey(EstadoTarea, on_delete = models.CASCADE)
